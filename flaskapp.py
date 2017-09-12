a5c2472fa272e8a75b96f8d86c870bd5db42a23a
nickage87 <nickage@mail.ru>
2017-09-12 04:35:06 -0400

    Added new line

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! \n v. 1.0'
+    return 'Hello, World! \n v. 1.0\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
